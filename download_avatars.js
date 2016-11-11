5872c4b38cffa6e440be1b092beaaefcb1d0ac52
Claire Gallant <claire.luna.gallant@gmail.com>
2016-11-11 00:27:01 +0000

    adding changes to download_avatars.js - scrapped it all and started over again

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,16 @@
-// var request = require('request');
-//
-// request.get('http://www.example.com', function(error, response, body) {
-//   console.log(arguments[2] === body);
-// });
-//
+var request = require('request');
+var fs = require('fs');
 
+
+// https://github.com/JellyGlow/excercise_requestX
+// these are already the two http string paths you'd want to
+// tag onto the address as a ressource path
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+  .on('error', function (err) {
+         throw err;
+       })
+  .on('response', function (response) {
+         console.log('Response Status Code: %s, Content type: %s ', response.statusMessage, response.headers['content-type']);
+       })
+  .pipe(fs.createWriteStream('./future.jpg'));
